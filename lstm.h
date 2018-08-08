a97047a8a15c1e2d14984767b8101f77c8581345
Christopher Aberger <christopheraberger@Christophers-MacBook-Pro-2.local>
2018-08-08 15:58:31 -0700

    Rewrote LSTM cell as a single GEMM.

--- lstm.h
+++ lstm.h
@@ -23,6 +23,7 @@ struct LSTM {
   float* g_t;
   float* o_t;
   float* x_h_in_cat;
+  float* buffers;
 
   const int input_size;
   const int hidden_size;
@@ -46,11 +47,13 @@ struct LSTM {
     // Allocate parameters.
     weights_l = (float*)malloc(sizeof(float) * 4 * hidden_size *
                                (input_size + hidden_size));
+    memset(weights_l, 0, sizeof(float) * 4 * hidden_size *
+                               (input_size + hidden_size));
     bias_ih_l = (float*)malloc(sizeof(float) * 4 * hidden_size);
     bias_hh_l = (float*)malloc(sizeof(float) * 4 * hidden_size);
 
     // Allocate intermediate buffers.
-    f_t = (float*)malloc(sizeof(float) * batch_size * hidden_size);
+    buffers = (float*)malloc(4 * sizeof(float) * batch_size * hidden_size);
     i_t = (float*)malloc(sizeof(float) * batch_size * hidden_size);
     g_t = (float*)malloc(sizeof(float) * batch_size * hidden_size);
     o_t = (float*)malloc(sizeof(float) * batch_size * hidden_size);
@@ -59,12 +62,18 @@ struct LSTM {
 
     if (input_size == 3 && hidden_size == 3) {
       std::vector<float> weight_ih_l_data = {
-          0.2975,  -0.2548, -0.1119, 0.2710, -0.5435, 0.3462,  -0.1188, 0.2937,
-          0.0803,  -0.0707, 0.1601,  0.0285, 0.2109,  -0.2250, -0.0421, -0.0520,
-          0.0837,  -0.0023, 0.5047,  0.1797, -0.2150, -0.3487, -0.0968, -0.2490,
-          -0.1850, 0.0276,  0.3442,  0.3138, -0.5644, 0.3579,  0.1613,  0.5476,
-          0.3811,  -0.5260, -0.5489, -0.2785};
-
+          0.2975,  -0.2548, -0.1119, 
+          0.2710, -0.5435, 0.3462,  
+          -0.1188, 0.2937, 0.0803,  
+          -0.0707, 0.1601,  0.0285, 
+          0.2109,  -0.2250, -0.0421, 
+          -0.0520, 0.0837,  -0.0023, 
+          0.5047,  0.1797, -0.2150, 
+          -0.3487, -0.0968, -0.2490,
+          -0.1850, 0.0276,  0.3442,  
+          0.3138, -0.5644, 0.3579,  
+          0.1613,  0.5476, 0.3811,  
+          -0.5260, -0.5489, -0.2785};
       /*
       std::vector<float> weight_ih_l_data = {
          0.2975, -0.2548, -0.1119,
@@ -89,7 +98,7 @@ struct LSTM {
       1,  1, 1,  1, 1, 1, 1, 1,
       1, 1,  1,  1, 1, 1,  1,  1,
       1,  1, 1, 1};
-*/
+      */
 
       std::vector<float> weight_hh_l_data = {
           0.5070,  -0.0962, 0.2471,  -0.2683, 0.5665,  -0.2443,
@@ -108,67 +117,47 @@ struct LSTM {
                                            0.1160,  0.1387,  -0.3866, -0.2739};
 
       /*
-      std::vector<float> bias_ih_l_data = {0,  0,  0,  0,
-                                           0,  0,  0,  0,
-                                           0,  0,  0,  0};
+      std::vector<float> bias_ih_l_data = {1,  1,  1,  1,
+                                           1,  1,  1,  1,
+                                           1,  1,  1,  1};
 
-      std::vector<float> bias_hh_l_data = {0,  0,  0,  0,
-                                           0,  0,  0,  0,
-                                           0,  0,  0,  0};
+      std::vector<float> bias_hh_l_data = {1,  1,  1,  1,
+                                           1,  1,  1,  1,
+                                           1,  1,  1,  1};
+
       */
-
       size_t input_copy_size = hidden_size * input_size;
       size_t input_offset = (input_size + hidden_size) * hidden_size;
       size_t hidden_copy_size = hidden_size * hidden_size;
 
-      memcpy(weights_l, weight_ih_l_data.data(),
-             sizeof(float) * input_copy_size);
-      memcpy(&weights_l[input_offset],
-             &weight_ih_l_data.data()[input_copy_size],
-             sizeof(float) * input_copy_size);
-      memcpy(&weights_l[2 * input_offset],
-             &weight_ih_l_data.data()[2 * input_copy_size],
-             sizeof(float) * input_copy_size);
-      memcpy(&weights_l[3 * input_offset],
-             &weight_ih_l_data.data()[3 * input_copy_size],
-             sizeof(float) * input_copy_size);
-
-      memcpy(&weights_l[input_copy_size], weight_hh_l_data.data(),
-             sizeof(float) * hidden_copy_size);
-      memcpy(&weights_l[input_copy_size + input_offset],
-             &weight_hh_l_data.data()[hidden_copy_size],
-             sizeof(float) * hidden_copy_size);
-      memcpy(&weights_l[input_copy_size + 2 * input_offset],
-             &weight_hh_l_data.data()[2 * hidden_copy_size],
-             sizeof(float) * hidden_copy_size);
-      memcpy(&weights_l[input_copy_size + 3 * input_offset],
-             &weight_hh_l_data.data()[3 * hidden_copy_size],
-             sizeof(float) * hidden_copy_size);
 
       // Take matrix and transpose it.
-
       for (size_t i = 0; i < input_size; ++i) {
         for (size_t j = 0; j < hidden_size; ++j) {
-          weights_l[i * hidden_size + j] =
+          weights_l[i * hidden_size * 4 + j] =
               weight_ih_l_data.data()[j * input_size + i];
-          weights_l[input_offset + i * hidden_size + j] =
+          weights_l[hidden_size + i * hidden_size * 4 + j] =
               weight_ih_l_data.data()[input_copy_size + j * input_size + i];
-          weights_l[2 * input_offset + i * hidden_size + j] =
+          weights_l[2 * hidden_size + i * hidden_size * 4 + j] =
               weight_ih_l_data.data()[2 * input_copy_size + j * input_size + i];
-          weights_l[3 * input_offset + i * hidden_size + j] =
+          weights_l[3 * hidden_size + i * hidden_size * 4 + j] =
               weight_ih_l_data.data()[3 * input_copy_size + j * input_size + i];
         }
       }
       for (size_t i = 0; i < hidden_size; ++i) {
         for (size_t j = 0; j < hidden_size; ++j) {
-          weights_l[input_copy_size + i * hidden_size + j] =
+          weights_l[input_copy_size * 4 + i * hidden_size * 4 + j] =
               weight_hh_l_data.data()[j * hidden_size + i];
-          weights_l[input_copy_size + input_offset + i * hidden_size + j] =
+          weights_l[hidden_size + input_copy_size * 4 + i * hidden_size * 4 +
+                    j] =
               weight_hh_l_data.data()[hidden_copy_size + j * hidden_size + i];
-          weights_l[input_copy_size + 2 * input_offset + i * hidden_size + j] =
+
+          weights_l[2 * hidden_size + 4 * input_copy_size +
+                    4 * i * hidden_size + j] =
               weight_hh_l_data
                   .data()[2 * hidden_copy_size + j * hidden_size + i];
-          weights_l[input_copy_size + 3 * input_offset + i * hidden_size + j] =
+          weights_l[3 * hidden_size + 4 * input_copy_size +
+                    i * hidden_size * 4 + j] =
               weight_hh_l_data
                   .data()[3 * hidden_copy_size + j * hidden_size + i];
         }
@@ -177,6 +166,14 @@ struct LSTM {
       memcpy(bias_ih_l, bias_ih_l_data.data(), sizeof(float) * 4 * hidden_size);
       memcpy(bias_hh_l, bias_hh_l_data.data(), sizeof(float) * 4 * hidden_size);
     }
+  }
+
+  void gemm(float* X, float* W, const int M, const int N,
+             const int K, float* buffer) {
+    // memcpy(buffer, bias, sizeof(float) * 4 * hidden_size);
+    const float bias_f = bias ? 1.0 : 0.0;
+    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, M, N, K, 1.0, X, K,
+                W, N, bias_f, buffer, N);
   }
 
   void layer(float* X, float* W, float* bias, const int M, const int N,
@@ -212,53 +209,50 @@ struct LSTM {
       }
     }
 
-    // We might not always need to do this if the input comes in concatenated.
-    const size_t weights_index = (input_size + hidden_size) * hidden_size;
-
+    // Copy bias term into place.
     for (size_t i = 0; i < batch_size; ++i) {
       for (size_t j = 0; j < hidden_size; ++j) {
-        i_t[i * hidden_size + j] = bias_ih_l[j] + bias_hh_l[j];
-      }
-    }
-    print_matrix(i_t, batch_size, hidden_size);
-    layer(x_h_in_cat, &weights_l[weights_index * 0], bias_ih_l, batch_size,
-          hidden_size, (input_size + hidden_size), i_t, fp_sigmoid);
+        buffers[i * hidden_size * 4 + j] =
+            bias_ih_l[0*hidden_size + j] + bias_hh_l[0* hidden_size + j];
+        buffers[hidden_size + i * hidden_size * 4 + j] =
+            bias_ih_l[1*hidden_size + j] + bias_hh_l[1* hidden_size + j];
 
-    for (size_t i = 0; i < batch_size; ++i) {
-      for (size_t j = 0; j < hidden_size; ++j) {
-        f_t[i * hidden_size + j] =
-            bias_ih_l[hidden_size + j] + bias_hh_l[hidden_size + j];
-      }
-    }
-    layer(x_h_in_cat, &weights_l[weights_index * 1], bias_ih_l, batch_size,
-          hidden_size, (input_size + hidden_size), f_t, fp_sigmoid);
-
-    for (size_t i = 0; i < batch_size; ++i) {
-      for (size_t j = 0; j < hidden_size; ++j) {
-        g_t[i * hidden_size + j] =
+        buffers[2 * hidden_size + i * hidden_size * 4 + j] =
             bias_ih_l[2*hidden_size + j] + bias_hh_l[2* hidden_size + j];
-      }
-    }
-    layer(x_h_in_cat, &weights_l[weights_index * 2], bias_ih_l, batch_size,
-          hidden_size, (input_size + hidden_size), g_t, fp_tanh);
 
-    for (size_t i = 0; i < batch_size; ++i) {
-      for (size_t j = 0; j < hidden_size; ++j) {
-        o_t[i * hidden_size + j] =
+        buffers[3 * hidden_size + i * hidden_size * 4 + j] =
             bias_ih_l[3*hidden_size + j] + bias_hh_l[3* hidden_size + j];
       }
     }
-    layer(x_h_in_cat, &weights_l[weights_index * 3], bias_ih_l, batch_size,
-          hidden_size, (input_size + hidden_size), o_t, fp_sigmoid);
+
+    print_matrix(buffers, batch_size, 4 * hidden_size);
+
+    print_matrix(x_h_in_cat, batch_size, input_size + hidden_size);
+
+    print_matrix(weights_l, input_size + hidden_size, hidden_size * 4);
+
+
+    gemm(x_h_in_cat, weights_l, batch_size, 4 * hidden_size,
+         input_size + hidden_size, buffers);
+
+    print_matrix(buffers, batch_size, 4* hidden_size);
+
 
     for (int i = 0; i < batch_size; ++i) {
       for (int j = 0; j < hidden_size; ++j) {
-        const size_t index = i * hidden_size + j;
+        const float i_t = fp_sigmoid(buffers[i * hidden_size * 4 + j]);
+        const float f_t =
+            fp_sigmoid(buffers[hidden_size + i * hidden_size * 4 + j]);
+        const float g_t =
+            fp_tanh(buffers[2 * hidden_size + i * hidden_size * 4 + j]);
+        const float o_t =
+            fp_sigmoid(buffers[3 * hidden_size + i * hidden_size * 4 + j]);
 
-        const float tmp1 = f_t[index] * C[index];
-        const float tmp2 = i_t[i * hidden_size + j] * g_t[i * hidden_size + j];
-        c_t[index] = tmp1 + tmp2;
-        h_t[index] = tanh(c_t[index]) * o_t[index];
+        const float tmp1 = f_t * C[i * hidden_size + j];
+        const float tmp2 = i_t * g_t;
+        c_t[i * hidden_size + j] = tmp1 + tmp2;
+        h_t[i * hidden_size + j] =
+            fp_tanh(c_t[i * hidden_size + j]) * o_t;
       }
     }
     return std::make_tuple(h_t, c_t);
