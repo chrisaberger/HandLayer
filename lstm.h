f511d6d7362493522981654dc8b9ad87e9f80416
Christopher Aberger <christopheraberger@Christophers-MacBook-Pro-2.local>
2018-08-08 11:28:19 -0700

    Matching pytorch lstm.

--- lstm.h
+++ lstm.h
@@ -10,8 +10,7 @@ namespace {
 }
 
 struct LSTM {
-  float* weight_ih_l;
-  float* weight_hh_l;
+  float* weights_l;
   float* bias_ih_l;
   float* bias_hh_l;
 
@@ -33,27 +32,61 @@ struct LSTM {
         batch_first(batch_first),
         dropout(dropout),
         bidirectional(bidirectional) {
-    weight_ih_l = (float*)malloc(sizeof(float) * 4 * hidden_size * input_size);
-    weight_hh_l = (float*)malloc(sizeof(float) * 4 * hidden_size * hidden_size);
+    weights_l = (float*)malloc(sizeof(float) * 4 * hidden_size *
+                               (input_size + hidden_size));
 
     bias_ih_l = (float*)malloc(sizeof(float) * 4 * hidden_size);
     bias_hh_l = (float*)malloc(sizeof(float) * 4 * hidden_size);
 
     if (input_size == 3 && hidden_size == 3) {
+
       std::vector<float> weight_ih_l_data = {
           0.2975,  -0.2548, -0.1119, 0.2710, -0.5435, 0.3462,  -0.1188, 0.2937,
           0.0803,  -0.0707, 0.1601,  0.0285, 0.2109,  -0.2250, -0.0421, -0.0520,
           0.0837,  -0.0023, 0.5047,  0.1797, -0.2150, -0.3487, -0.0968, -0.2490,
           -0.1850, 0.0276,  0.3442,  0.3138, -0.5644, 0.3579,  0.1613,  0.5476,
           0.3811,  -0.5260, -0.5489, -0.2785};
+          
+      /*
+      std::vector<float> weight_ih_l_data = {
+         0.2975, -0.2548, -0.1119,
+         0.2710, -0.5435,  0.3462,  
+         -0.1188,  0.2937,  0.0803, 
+         1,  1, 1, 
+          1,  1, 1,  
+          1,  1, 1,  
+          0.5047,  0.1797, -0.2150,
+          -0.3487, -0.0968, -0.2490,  
+          -0.1850,  0.0276,  0.3442,  
+          1,  1, 1, 
+          1,  1, 1,  
+          1,  1, 1, 
+        };
+        */
+
+
+          /*
+      std::vector<float> weight_ih_l_data = {
+          1,  1, 1, 1, 1, 1,  1, 1,
+          1,  1, 1,  1, 1,  1, 1, 1,
+          1,  1, 1,  1, 1, 1, 1, 1,
+          1, 1,  1,  1, 1, 1,  1,  1,
+          1,  1, 1, 1};
+    */
 
       std::vector<float> weight_hh_l_data = {
-          0.5070,  -0.0962, 0.2471,  -0.2683, 0.5665,  -0.2443,
-          0.4330,  0.0068,  -0.3042, 0.2968,  -0.3065, 0.1698,
-          -0.1667, -0.0633, -0.5551, -0.2753, 0.3133,  -0.1403,
-          0.5751,  0.4628,  -0.0270, -0.3854, 0.3516,  0.1792,
-          -0.3732, 0.3750,  0.3505,  0.5120,  -0.3236, -0.0950,
-          -0.0112, 0.0843,  -0.4382, -0.4097, 0.3141,  -0.1354};
+          0.5070,  -0.0962, 0.2471,  
+          -0.2683, 0.5665,  -0.2443,
+          0.4330,  0.0068,  -0.3042, 
+          0.2968,  -0.3065, 0.1698,
+          -0.1667, -0.0633, -0.5551, 
+          -0.2753, 0.3133,  -0.1403,
+          0.5751,  0.4628,  -0.0270, 
+          -0.3854, 0.3516,  0.1792,
+          -0.3732, 0.3750,  0.3505,  
+          0.5120,  -0.3236, -0.0950,
+          -0.0112, 0.0843,  -0.4382, 
+          -0.4097, 0.3141,  -0.1354};
 
       std::vector<float> bias_ih_l_data = {0.2820,  0.0329,  0.1896,  0.1270,
                                            0.2099,  0.2862,  -0.5347, 0.2906,
@@ -63,45 +96,167 @@ struct LSTM {
                                            -0.0856, 0.3247,  0.1856,  -0.4329,
                                            0.1160,  0.1387,  -0.3866, -0.2739};
 
-      memcpy(weight_ih_l, weight_ih_l_data.data(),
-             sizeof(float) * 4 * hidden_size * input_size);
-      memcpy(weight_hh_l, weight_hh_l_data.data(),
-             sizeof(float) * 4 * hidden_size * hidden_size);
+      size_t input_copy_size = hidden_size * input_size;
+      size_t input_offset = (input_size + hidden_size) * hidden_size;
+      size_t hidden_copy_size = hidden_size * hidden_size;
+
+      memcpy(weights_l, weight_ih_l_data.data(),
+             sizeof(float) * input_copy_size);
+      memcpy(&weights_l[input_offset],
+             &weight_ih_l_data.data()[input_copy_size],
+             sizeof(float) * input_copy_size);
+      memcpy(&weights_l[2 * input_offset],
+             &weight_ih_l_data.data()[2 * input_copy_size],
+             sizeof(float) * input_copy_size);
+      memcpy(&weights_l[3 * input_offset],
+             &weight_ih_l_data.data()[3 * input_copy_size],
+             sizeof(float) * input_copy_size);
+
+      std::cout << input_copy_size << std::endl;
+      memcpy(&weights_l[input_copy_size], weight_hh_l_data.data(),
+             sizeof(float) * hidden_copy_size);
+      memcpy(&weights_l[input_copy_size + input_offset],
+             &weight_hh_l_data.data()[hidden_copy_size],
+             sizeof(float) * hidden_copy_size);
+      memcpy(&weights_l[input_copy_size + 2 * input_offset],
+             &weight_hh_l_data.data()[2 * hidden_copy_size],
+             sizeof(float) * hidden_copy_size);
+      memcpy(&weights_l[input_copy_size + 3 * input_offset],
+             &weight_hh_l_data.data()[3 * hidden_copy_size],
+             sizeof(float) * hidden_copy_size);
+
+      // Take matrix and transpose it.
+
+      for (size_t i = 0; i < input_size; ++i) {
+        for (size_t j = 0; j < hidden_size; ++j) {
+          weights_l[i * hidden_size + j] = weight_ih_l_data.data()[j*input_size + i];
+          weights_l[input_offset + i * hidden_size + j] = weight_ih_l_data.data()[input_copy_size + j*input_size + i];
+          weights_l[2*input_offset + i * hidden_size + j] = weight_ih_l_data.data()[2*input_copy_size + j*input_size + i];
+          weights_l[3*input_offset + i * hidden_size + j] = weight_ih_l_data.data()[3*input_copy_size + j*input_size + i];
+        }
+      }
+      for (size_t i = 0; i < hidden_size; ++i) {
+        for (size_t j = 0; j < hidden_size; ++j) {
+          weights_l[input_copy_size+ i * hidden_size + j] = weight_hh_l_data.data()[j*hidden_size + i];
+          weights_l[input_copy_size + input_offset + i * hidden_size + j] = weight_hh_l_data.data()[hidden_copy_size + j*hidden_size + i];
+          weights_l[input_copy_size + 2*input_offset + i * hidden_size + j] = weight_hh_l_data.data()[2*hidden_copy_size + j*hidden_size + i];
+          weights_l[input_copy_size + 3*input_offset + i * hidden_size + j] = weight_hh_l_data.data()[3*hidden_copy_size + j*hidden_size + i];
+        }
+      }
+
+      print_matrix(weights_l, 4*(hidden_size+input_size), hidden_size);
+
+      /*
       memcpy(bias_ih_l, bias_ih_l_data.data(), sizeof(float) * 4 * hidden_size);
       memcpy(bias_hh_l, bias_hh_l_data.data(), sizeof(float) * 4 * hidden_size);
+      */
     }
   }
 
   void layer(float* X, float* W, float* bias, const int M, const int N,
-                     const int K, float* buffer, std::function<float(float)> f) {
-    //memcpy(buffer, bias, sizeof(float) * 4 * hidden_size);
-
-    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasTrans, M, N, K, 1.0, X, K,
-                W, N, 0.0, buffer, N);
+             const int K, float* buffer, std::function<float(float)> f) {
+    // memcpy(buffer, bias, sizeof(float) * 4 * hidden_size);
+    std::cout << M << " " << N << " " << K << std::endl;
+    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, M, N, K, 1.0, 
+                X, K, 
+                W, N, 0.0, 
+                buffer, N);
     for (int i = 0; i < M; ++i) {
       for (int j = 0; j < N; ++j) {
-        std::cout << buffer[i * input_size + j] << " " << f(buffer[i * input_size + j]) << std::endl;
-        buffer[i * input_size + j] = f(buffer[i * input_size + j]);
+        buffer[i * N + j] = f(buffer[i * N + j]);
       }
     }
   }
 
-  void forward(float* X, int batch_size){
-    float* state_g = (float*)malloc(sizeof(float) * batch_size * hidden_size);
+  void forward(float* X, float * H, float * C, int batch_size){
+    float* f_t = (float*)malloc(sizeof(float) * batch_size * hidden_size);
+    memset(f_t, 0, sizeof(float) * batch_size * hidden_size);
 
-    print_matrix(bias_ih_l, 1, hidden_size);
+    float* i_t = (float*)malloc(sizeof(float) * batch_size * hidden_size);
+    memset(i_t, 0, sizeof(float) * batch_size * hidden_size);
+    float* g_t = (float*)malloc(sizeof(float) * batch_size * hidden_size);
+    memset(g_t, 0, sizeof(float) * batch_size * hidden_size);
+    float* o_t = (float*)malloc(sizeof(float) * batch_size * hidden_size);
+    memset(o_t, 0, sizeof(float) * batch_size * hidden_size);
 
+    float* x_h_in_cat =
+        (float*)malloc(sizeof(float) * batch_size * (hidden_size + input_size));
+    float* h_t = (float*)malloc(sizeof(float) * batch_size * hidden_size);
+    float* c_t = (float*)malloc(sizeof(float) * batch_size * hidden_size);
+
+    // Copy into x_h.
+    for (size_t i = 0; i < batch_size; ++i) {
+      for (size_t j = 0; j < input_size; ++j) {
+        x_h_in_cat[i * (input_size + hidden_size) + j] = X[i * input_size + j];
+      }
+    }
+    for (size_t i = 0; i < batch_size; ++i) {
+      for (size_t j = 0; j < hidden_size; ++j) {
+        x_h_in_cat[i * (input_size + hidden_size) + j + input_size] =
+            H[i * hidden_size + j];
+      }
+    }
+
+    std::cout << "" << std::endl;
+    print_matrix(x_h_in_cat, batch_size, input_size+hidden_size);
+
+    // We might not always need to do this if the input comes in concatenated.
+    
+    std::cout << "Running layers..." << std::endl;
+
+    const size_t weights_index = (input_size+hidden_size)*hidden_size;
+    print_matrix(&weights_l[weights_index], input_size+hidden_size, hidden_size);
+
+    layer(x_h_in_cat, &weights_l[weights_index * 0], bias_ih_l, batch_size,
+          hidden_size, (input_size + hidden_size), i_t, sigmoid);
+    std::cout << "i_T" << std::endl;
+    print_matrix(i_t, batch_size, hidden_size);
+
+    layer(x_h_in_cat, &weights_l[weights_index * 1],
+          bias_ih_l, batch_size, hidden_size, (input_size + hidden_size), f_t,
+          sigmoid);
+
+    layer(x_h_in_cat, &weights_l[weights_index * 2],
+          bias_ih_l, batch_size, hidden_size, (input_size + hidden_size), g_t,
+          [](float a) { return tanh(a); });
+
+    layer(x_h_in_cat, &weights_l[weights_index * 3],
+          bias_ih_l, batch_size, hidden_size, (input_size + hidden_size), o_t,
+          sigmoid);
+
+
+    //print_matrix(o_t, batch_size, hidden_size);
+
+    for (int i = 0; i < batch_size; ++i) {
+      for (int j = 0; j < hidden_size; ++j) {
+        const size_t index = i * hidden_size + j;
+
+        const float tmp1 = f_t[index] * C[index];
+        const float tmp2 = i_t[i * hidden_size + j] * g_t[i * hidden_size + j];
+        c_t[index] = tmp1+tmp2;
+        h_t[index] = tanh(c_t[index]) * o_t[index];
+      }
+    }
+
+
+    std::cout << "h_t" << std::endl;
+    print_matrix(h_t, batch_size, hidden_size);
+    std::cout << "c_t" << std::endl;
+    print_matrix(c_t, batch_size, hidden_size);
+
+    /*
     std::cout << " " << std::endl;
     print_matrix(X, batch_size, input_size);
 
     std::cout << " " << std::endl;
     print_matrix(weight_ih_l, hidden_size, input_size);
 
-    layer(weight_ih_l, X, bias_ih_l, batch_size, hidden_size,
-                  input_size, state_g, sigmoid);
+    //layer(weight_ih_l, X, bias_ih_l, batch_size, hidden_size,
+    //              input_size, state_g, sigmoid);
 
-    print_matrix(state_g, batch_size, hidden_size);
+    */
     std::cout << "End forward" << std::endl;
+
   }
 
 
--- main.cc
+++ main.cc
@@ -15,11 +15,34 @@ int main()
     -2.5667, -1.4303,  0.5009,
     0.5438, -0.4057,  1.1341 };
 
+  std::vector<float> h = {
+    -0.6331,  0.8795, -0.6842,
+    0.4533,  0.2912, -0.8317
+  };
+
+  std::vector<float> c = {
+    -0.5525,  0.6355, -0.3968,
+    -0.6571, -1.6428,  0.9803
+  };
+
+  /*
+  std::vector<float> h = {
+    0,  0, 0,
+    0,  0, 0
+  };
+
+  std::vector<float> c = {
+    0,  0, 0,
+    0, 0,  0
+  };
+  */
+
+
   int input_size = 3;
   int batch_size = 2;
   int n_hidden = 3;
   LSTM lstm = LSTM(input_size, n_hidden);
-  lstm.forward(input_data.data(), batch_size);
+  lstm.forward(input_data.data(), h.data(), c.data(), batch_size);
   //lstm.print_matrix(lstm.weight_ih_l, 4 * n_hidden, input_size);
   return 0;
 }
