babb5894250f7771270b6b56fe69a02288133bc5
Christopher Aberger <christopheraberger@Christophers-MacBook-Pro-2.local>
2018-08-13 20:28:17 -0700

    Changing back types.

--- src/types/float32.h
+++ src/types/float32.h
@@ -8,7 +8,7 @@
 
 inline float sigmoid(const float in) { return 1.0 / (1.0 + exp(-in)); }
 inline void gemm(float* X, float* W, const int M, const int N, const int K,
-                 float* buffer, bool bias, const std::string type = "") {
+                 float* buffer, bool bias) {
   const float bias_f = bias ? 1.0 : 0.0;
   cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, M, N, K, 1.0, X, K, W,
               N, bias_f, buffer, N);
